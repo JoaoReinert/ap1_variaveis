b7521c9e2db0c9571bb6746855bca5a954f8c8a2
Joao Reinert <joao33reinert@gmail.com>
2024-03-23 15:22:13 -0300

    media

---
+++ ap2.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  const double num1 = 7.0;
+  const double num2 = 8.0;
+  const double num3 = 5.3;
+  const double num4 = 9.7;
+
+  final double media = (num1 + num2 + num3 + num4) / 4;
+
+  print("A media entre [ $num1 , $num2 , $num3 , $num4 ] = $media");
+}
