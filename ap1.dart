fa9f4189355fc651952408259c0200623fa79cfc
Joao Reinert <joao33reinert@gmail.com>
2024-03-23 00:14:16 -0300

    first commit

---
+++ ap1.dart
@@ -0,0 +1,34 @@
+void main() {
+  String nome = "João";
+  String sobrenome = "Reinert";
+
+  int idade = 18;
+
+  bool situacao = true;
+
+  double peso = 61.6;
+
+  String? nacionalidade = null;
+
+  print("Nome completo: $nome $sobrenome");
+
+  if (idade >= 18) {
+    print("Idade: $idade, vc é de maior");
+  } else {
+    print("Idade: $idade, vc é de menor");
+  }
+
+  if (situacao) {
+    print("Situação: Ativo");
+  } else {
+    print("Situação: Inativo");
+  }
+
+  print("Peso: $peso");
+
+  if (nacionalidade == null) {
+    print("Nacionalidade: Nao informada");
+  } else {
+    print("Nacionalidade: $nacionalidade ");
+  }
+}
